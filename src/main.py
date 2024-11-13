ee1b109dc5aed381c1f0c47654a3afcb2e8658cb
Nickolay Malyshev <nickolaymalyshevjr@gmail.com>
2024-11-13 20:19:18 +0200

    adding docker

--- src/main.py
+++ src/main.py
@@ -57,7 +57,7 @@ async def authenticated_route(user: User = Depends(current_active_user)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 
